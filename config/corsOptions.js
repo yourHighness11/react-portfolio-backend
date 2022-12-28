11bc9652d6ff57e356cf796b7742cf193e322f6c
Sarvesh <coolsarveshkirodiwal111@gmail.com>
2022-12-28 17:35:37 +0530

    origin updated

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,10 @@
-const corsOptions = {
-  origin: process.env.ORIGIN_LINK,
-  optionsSuccessStatus: 200,
-  method: "POST",
+const whitelist = ["https://sarvesh-portfolio-19a99.web.app"];
+const corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
 };
-
-module.exports = corsOptions;
